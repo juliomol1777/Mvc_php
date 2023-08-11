cc51444072c1d8ce8a1f0e0cccec5bbdc6e5797f
Julio M <juliomolina17@gmail.com>
2023-08-11 01:34:55 -0300

    armando estructucta del proyecto

---
+++ config/app.php
@@ -0,0 +1,6 @@
+<?php
+
+    const APP_URL="http://localhost/mvc_php/";
+    const APP_NAME="JulioM";
+    const APP_SESSION_NAME="CRUD";
+    date_default_timezone_set("America/Argentina/Buenos_Aires");
